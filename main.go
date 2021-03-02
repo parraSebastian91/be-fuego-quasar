267587eed410fdfd3bb5c966d4bc190a126670ea
Sebastian Parra <parra.sebastian91@gmail.com>
2021-03-02 14:01:05 -0300

    GetLocation ok

--- main.go
+++ main.go
@@ -24,6 +24,73 @@ type satellites struct {
 	satellites []satellite
 }
 
+func main() {
+	s := satellites{
+		satellites: []satellite{
+			{
+				name:     "kenobi",
+				distance: 485.69,
+				massage:  []string{"este", "", "", "mensaje", ""},
+			}, {
+				name:     "skywalker",
+				distance: 266.08,
+				massage:  []string{"", "es", "", "", "secreto"},
+			}, {
+				name:     "sato",
+				distance: 600.5,
+				massage:  []string{"este", "", "un", "", ""},
+			},
+		},
+	}
+	fmt.Println(GetLocation(s))
+}
+
+//
+func GetLocation(distances satellites) coordenada {
+
+	var kenobi circulo
+	var skywalker circulo
+	var sato circulo
+	for _, e := range distances.satellites {
+		if e.name == "kenobi" {
+			kenobi = circulo{
+				puntoCentro: coordenada{x: -500, y: -200},
+				radio:       e.distance,
+			}
+		} else if e.name == "skywalker" {
+			skywalker = circulo{
+				puntoCentro: coordenada{x: 100, y: -100},
+				radio:       e.distance,
+			}
+		} else if e.name == "sato" {
+			sato = circulo{
+				puntoCentro: coordenada{x: 500, y: 100},
+				radio:       e.distance,
+			}
+		}
+	}
+
+	punto1, punto2 := distances.intersecion_entre_circunferencias(kenobi, skywalker)
+	punto3, punto4 := distances.intersecion_entre_circunferencias(skywalker, sato)
+	punto5, punto6 := distances.intersecion_entre_circunferencias(kenobi, sato)
+	arrayCoordenadas := []coordenada{punto1, punto2, punto3, punto4, punto5, punto6}
+	return filtrarCoordenada(arrayCoordenadas)
+}
+
+func filtrarCoordenada(arr []coordenada) coordenada {
+	dict := make(map[coordenada]int)
+	var coorBuscada coordenada = arr[0]
+	for _, punto := range arr {
+		dict[punto] = dict[punto] + 1
+	}
+	for key, element := range dict {
+		if dict[coorBuscada] < element {
+			coorBuscada = key
+		}
+	}
+	return coorBuscada
+}
+
 func getDistanciaEntreCirculos(c1 circulo, c2 circulo) float64 {
 	dxAB, dyAB := c2.puntoCentro.x-c1.puntoCentro.x, c2.puntoCentro.y-c1.puntoCentro.y
 	dAB := math.Sqrt(dxAB*dxAB + dyAB*dyAB)
@@ -56,49 +123,5 @@ func (c *satellites) intersecion_entre_circunferencias(cA circulo, cB circulo) (
 	xs2 := xm - h*dyAB/distanciaAB
 	ys1 := ym - h*dxAB/distanciaAB
 	ys2 := ym + h*dxAB/distanciaAB
-	return coordenada{x: xs1, y: ys1}, coordenada{x: xs2, y: ys2}
-}
-
-func main() {
-	s := satellites{
-		satellites: []satellite{
-			satellite{
-				name:     "kenobi",
-				distance: 485.69,
-				massage:  []string{"este", "", "", "mensaje", ""},
-			}, satellite{
-				name:     "skywalker",
-				distance: 266.08,
-				massage:  []string{"", "es", "", "", "secreto"},
-			}, satellite{
-				name:     "sato",
-				distance: 600.5,
-				massage:  []string{"este", "", "un", "", ""},
-			},
-		},
-	}
-
-	kenobi := circulo{
-		puntoCentro: coordenada{x: -500, y: -200},
-		radio:       s.satellites[0].distance,
-	}
-	skywalker := circulo{
-		puntoCentro: coordenada{x: 100, y: -100},
-		radio:       s.satellites[1].distance,
-	}
-	sato := circulo{
-		puntoCentro: coordenada{x: 500, y: 100},
-		radio:       s.satellites[2].distance,
-	}
-
-	punto1, punto2 := s.intersecion_entre_circunferencias(kenobi, skywalker)
-	punto3, punto4 := s.intersecion_entre_circunferencias(skywalker, sato)
-	punto5, punto6 := s.intersecion_entre_circunferencias(kenobi, sato)
-	fmt.Println("punto 1: (", math.Round(punto1.x), ",", math.Round(punto1.y), ")")
-	fmt.Println("punto2: (", math.Round(punto2.x), ",", math.Round(punto2.y), ")")
-	fmt.Println("punto 3: (", math.Round(punto3.x), ",", math.Round(punto3.y), ")")
-	fmt.Println("punto4: (", math.Round(punto4.x), ",", math.Round(punto4.y), ")")
-	fmt.Println("punto 5: (", math.Round(punto5.x), ",", math.Round(punto5.y), ")")
-	fmt.Println("punto6: (", math.Round(punto6.x), ",", math.Round(punto6.y), ")")
-
+	return coordenada{x: math.Round(xs1*10) / 10, y: math.Round(ys1*10) / 10}, coordenada{x: math.Round(xs2*10) / 10, y: math.Round(ys2*10) / 10}
 }
